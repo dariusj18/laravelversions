d851be66d334066e96679d687ef814ed49143b2e
Tyler Clendenin <tylerc@gslsolutions.com>
2021-01-26 12:58:03 -0500

    More lint

--- database/migrations/2021_01_26_144212_add_change_log.php
+++ database/migrations/2021_01_26_144212_add_change_log.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class AddChangeLog extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::table('laravel_versions', function (Blueprint $table) {
@@ -19,11 +14,6 @@ class AddChangeLog extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::table('laravel_versions', function (Blueprint $table) {
